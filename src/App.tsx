76e74032e77b688c731cd85d69c3ef64d6fb0b59
Volodymyr Nerovnia <nerv@i.ua>
2024-03-29 01:16:22 +0200

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from '@tanstack/react-query';
+
+const todoId = 1;
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const { isLoading, error, data } = useQuery(['todos', todoId], () => 
+    fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => 
+      response.json() 
+    )
   )
+  console.log(data);
+  return <div>{data ? <h1>Todo: {data.title}</h1> : <h1>Data not found!</h1>}</div>
 }
 
 export default App
