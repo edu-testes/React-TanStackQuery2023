47e6024c763f43d840624e3d73e81549815545fd
Volodymyr Nerovnia <nerv@i.ua>
2024-03-30 22:47:18 +0200

    App: useQuery is work, but axioos need improve

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,50 @@ const todoId = 1;
 
 function App() {
 
-  const { isLoading, error, data } = useQuery(
+  const { isLoading, error, data } = useQuery({ 
+    queryKey: ['todos'], 
+    queryFn: async() => {
+      await axios.get<ITodo>('https://jsonplaceholder.typicode.com/todos/1'),
+      {
+        select: ({ data }) => {
+          return data;
+        }
+      }
+    },} 
+  )
+  console.log(data);
+  return <div>{data ? <h1>Todo: {data}</h1> : <h1>Data not found!</h1>}</div>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App
+
+
+
+
+
+
+
+
+/*
+
     ['todos', todoId], () => 
     axios.get<ITodo>('https://jsonplaceholder.typicode.com/todos/1'),
     {
       select: ({ data }) => data,
     }
-  )
-  console.log(data);
-  return <div>{data ? <h1>Todo: {data.title}</h1> : <h1>Data not found!</h1>}</div>
-}
 
-export default App
+*/
