df13be4f8e77628e06f7481ccef62a3a818ca21b
Volodymyr Nerovnia <nerv@i.ua>
2024-03-30 22:04:35 +0200

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { ITodo } from './app.interface';
 
 const todoId = 1;
 
 function App() {
 
-  const { isLoading, error, data } = useQuery(['todos', todoId], () => 
-    fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => 
-      response.json() 
-    )
+  const { isLoading, error, data } = useQuery(
+    ['todos', todoId], () => 
+    axios.get<ITodo>('https://jsonplaceholder.typicode.com/todos/1'),
+    {
+      select: ({ data }) => data,
+    }
   )
   console.log(data);
   return <div>{data ? <h1>Todo: {data.title}</h1> : <h1>Data not found!</h1>}</div>
